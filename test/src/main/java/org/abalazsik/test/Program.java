11db1b11a5c83392e84662a63b9cc346fe354c69
ador <balazsik.adorjan@gmail.com>
2024-08-08 22:55:09 +0200

    override properties

--- test/src/main/java/org/abalazsik/test/Program.java
+++ test/src/main/java/org/abalazsik/test/Program.java
@@ -1,14 +1,18 @@
 
 package org.abalazsik.test;
 
+import java.io.InputStreamReader;
+
 /**
  *
  * @author ador
  */
 public class Program {
-	
+
 	public static void main(String[] args) {
 		System.out.println(String.format(hello.properties.template(), hello.properties.name()));
+		hello.properties.load(Program.class.getResourceAsStream("/hello-ext.properties"));
+		System.out.println(hello.properties.this_is_a_longer_property()); // new value
 	}
-	
+
 }
